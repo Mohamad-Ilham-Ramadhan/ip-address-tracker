229ba5bd2d4dab1b3ee9732edba93ab02d0e392c
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2020-09-08 19:54:53 +0700

    fix null

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
           setRegion(result.region);
           setCity(result.city);
           // setZip(result.zip); // ip-api
-          setZip(result.postal);
+          setZip(result.postal == null ? "" : result.postal);
           setTimezone(result.timezone);
           // setIsp(result.isp); // ip-api
           setIsp(result.org);
